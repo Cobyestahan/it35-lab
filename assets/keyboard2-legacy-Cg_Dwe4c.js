5b45475c29b30dc22f457607e9abbc6579a179af
Cobyestahan <estahanjacobjay@estahan>
2025-04-02 16:08:23 +0800

    Updates

--- assets/keyboard2-legacy-Cg_Dwe4c.js
+++ assets/keyboard2-legacy-Cg_Dwe4c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BScrJXiw.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Bgp1Mc9p.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
