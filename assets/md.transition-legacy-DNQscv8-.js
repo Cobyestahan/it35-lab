66114030ed4b70fb0318f86ffcbde19fea02627d
Cobyestahan <estahanjacobjay@gmail.com>
2025-04-30 10:51:19 -0700

    Updates

--- assets/md.transition-legacy-DNQscv8-.js
+++ assets/md.transition-legacy-DNQscv8-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D6C1X-OK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-mpuSwFVp.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
